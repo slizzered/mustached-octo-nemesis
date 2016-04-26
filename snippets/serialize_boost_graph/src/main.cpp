17ea2a96db8d1d4300589843cf25a45a7029d111
Carlchristian Eckert <Carli-Eckert@gmx.de>
2016-04-26 15:57:54 +0200

    improvement for graph to property tree

--- snippets/serialize_boost_graph/src/main.cpp
+++ snippets/serialize_boost_graph/src/main.cpp
@@ -13,6 +13,74 @@ struct MyEdge
     int id;
 };
 
+template<typename T_Graph>
+void
+addToPropertyTree(
+    boost::property_tree::ptree & pt,
+    std::string path,
+    T_Graph & g,
+    boost::dynamic_properties & dp
+)
+{
+    //convert graph to property_tree
+    boost::property_tree::ptree pt_graph;
+    std::stringstream ss;
+    boost::write_graphml(ss, g, dp);
+
+    // trim_whitespace is a workaround to avoid printing  &#10;  at each newline
+    // http://stackoverflow.com/questions/6572550/boostproperty-tree-xml-pretty-printing
+    // https://svn.boost.org/trac/boost/ticket/11600
+    boost::property_tree::read_xml(ss, pt_graph, boost::property_tree::xml_parser::trim_whitespace);
+
+    // add graph property tree to global pt
+    pt.add_child(path, pt_graph);
+}
+
+template<typename T_Graph>
+void
+getFromPropertyTree(
+    boost::property_tree::ptree & pt,
+    std::string path,
+    T_Graph & g,
+    boost::dynamic_properties & dp
+)
+{
+    //extract graph as property tree
+    boost::property_tree::ptree recovered_child = pt.get_child(path);
+
+    // write extracted graph as XML for boost bgl
+    std::stringstream ss;
+    boost::property_tree::xml_writer_settings<std::string> settings(' ', 4);
+    boost::property_tree::write_xml(ss, recovered_child, settings);
+
+    // read recovered XML as a boost BGL graph
+    boost::read_graphml(ss, g, dp);
+}
+
+void
+savePropertyTreeAsXML(
+    boost::property_tree::ptree & pt,
+    std::string path
+)
+{
+    std::ofstream fs;
+    fs.open(path);
+    boost::property_tree::xml_writer_settings<std::string> settings(' ', 4);
+    boost::property_tree::write_xml(fs, pt, settings);
+    fs.close();
+}
+
+void
+readPropertyTreeFromXML(
+    boost::property_tree::ptree & pt,
+    std::string path
+)
+{
+    std::ifstream fs(path);
+    read_xml(fs, pt, boost::property_tree::xml_parser::trim_whitespace);
+    fs.close();
+}
+
 int main()
 {
     using namespace boost;
@@ -33,7 +101,9 @@ int main()
     //convert graph to property_tree
     property_tree::ptree pt_graph;
     std::stringstream ss;
-    write_graphml(ss, g, dynamic_properties());
+    dynamic_properties dp1;
+    dp1.property("hid", boost::get(&MyVertex::id, g));
+    write_graphml(ss, g, dp1);
 
     // trim_whitespace is a workaround to avoid printing  &#10;  at each newline
     // http://stackoverflow.com/questions/6572550/boostproperty-tree-xml-pretty-printing
@@ -66,8 +136,9 @@ int main()
 
     // read recovered XML as a boost BGL graph
     adjacency_list<vecS, vecS, directedS, MyVertex, MyEdge> g2;
-    dynamic_properties dp;
-    read_graphml(ss2, g2, dp);
+    dynamic_properties dp2;
+    dp2.property("hid", boost::get(&MyVertex::id, g2));
+    read_graphml(ss2, g2, dp2);
     std::cout << ss2.str() << std::endl;
 
     return 0;
